47b33d42033e5d246eeafe182929aeedc91b6981
Jennifer King <jenbellpgh@gmail.com>
2019-05-29 02:12:25 -0400

    Added some additional functionality to Room

--- src/room.py
+++ src/room.py
@@ -7,11 +7,19 @@ class Room:
   def __init__(self, name, description):
     self.name = name
     self.description = description
+    self.n_to = None
+    self.s_to = None
+    self.w_to = None
+    self.e_to = None
 
   def __str__(self):
     output = f'"{self.name}"\n'
-    output += self.description + "\n"
-    return textwrap.fill(output)
+    output += textwrap.fill(self.description, 70, initial_indent='    ') + "\n"
+    return output
 
-room = Room("test","a test room")
-print(room)
+
+test = Room("test", "Just another test room with a bunch of descriptive words and stuff. I wonder what else we can add here, I'd like to make it pretty long. I guess this is good enough.")
+print(test)
+
+test.n_to = Room("another test", "Here we have another test room, but this one doesn't have as much detail to it.")
+print(test.n_to)
