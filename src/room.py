43e273cd99672b10903f43d91f6f159afac1a195
Jennifer King <jenbellpgh@gmail.com>
2019-05-29 01:49:45 -0400

    Setup Room

--- src/room.py
+++ src/room.py
@@ -1,2 +1,17 @@
+import textwrap
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+
+  def __init__(self, name, description):
+    self.name = name
+    self.description = description
+
+  def __str__(self):
+    output = f'"{self.name}"\n'
+    output += self.description + "\n"
+    return textwrap.fill(output)
+
+room = Room("test","a test room")
+print(room)
